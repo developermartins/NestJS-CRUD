6135abb9b988c1864912c0ff82bc78a4cf3ff77a
martsDev <martssilvapro@gmail.com>
2022-02-10 11:51:43 -0300

    adiciona projeto

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URL),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
